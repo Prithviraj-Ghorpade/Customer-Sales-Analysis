2b56b279e907e0781fe413614defeb919030c656
Prithviraj Ghorpade <prithvighorpade653@gmail.com>
2025-04-29 18:48:26 +0530

    Update revenue_by_year-month.sql

--- Order and Sales Analysis/revenue_by_year-month.sql
+++ Order and Sales Analysis/revenue_by_year-month.sql
@@ -1,3 +1,4 @@
+/*Shows Year-Month Wise Revenue*/
 SELECT strftime('%Y-%m', order_date) AS order_yearmonth, SUM(order_amount) AS total_revenue
 FROM customer_orders
 WHERE order_status = 'delivered'
