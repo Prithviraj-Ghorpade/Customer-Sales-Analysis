227841dab4c36e8cc9eeb04d475d5aa4dba22fa2
Prithviraj Ghorpade <prithvighorpade653@gmail.com>
2025-04-29 18:24:34 +0530

    Delete revenue_by_year.sql

--- revenue_by_year.sql
+++
@@ -1,5 +0,0 @@
-SELECT strftime('%Y', order_date) AS order_year, SUM(order_amount) AS total_revenue
-FROM customer_orders
-WHERE order_status = 'delivered'
-GROUP BY order_year
-ORDER BY order_year;
