e66e5d8a06ee659a044a0146ec779e12f7833c22
Prithviraj Ghorpade <prithvighorpade653@gmail.com>
2025-04-29 18:49:42 +0530

    Update order_status_analysis.sql

--- Order and Sales Analysis/order_status_analysis.sql
+++ Order and Sales Analysis/order_status_analysis.sql
@@ -1,4 +1,4 @@
-/*How many orders are pending, shipped, or delivered*/
+/*Shows How many orders are pending, shipped, or delivered*/
 SELECT order_status, COUNT(*) AS total_orders
 FROM customer_orders
 GROUP BY order_status;
