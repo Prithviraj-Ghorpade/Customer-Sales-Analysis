cd735b2f7d9fec24e0c465ef6f19694b0c87fa19
Prithviraj Ghorpade <prithvighorpade653@gmail.com>
2025-04-29 18:43:55 +0530

    Add files via upload

---
+++ Order Details Report/order_details_report.sql
@@ -0,0 +1,16 @@
+SELECT o.customer_id, o.order_id, p.payment_id, o.order_status, o.order_date, p.payment_date, 
+CASE 
+    WHEN JULIANDAY(p.payment_date) > JULIANDAY(o.order_date)
+    THEN JULIANDAY(p.payment_date) - JULIANDAY(o.order_date)
+    ELSE NULL
+END AS days_to_payment,
+o.order_amount, p.payment_amount, p.payment_method, 
+CASE 
+	WHEN p.payment_status = 'completed' THEN 'Paid'
+	WHEN p.payment_status = 'pending' THEN 'Pending'
+	WHEN p.payment_status = 'failed' THEN 'Failed'
+	ELSE 'Unknown'
+END AS payment_status_label
+FROM customer_orders o
+LEFT JOIN payments p 
+ON o.order_id = p.order_id;
