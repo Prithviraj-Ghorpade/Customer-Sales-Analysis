b6e471fa8180e1d2577b9c2be577e535ec0d266a
Prithviraj Ghorpade <prithvighorpade653@gmail.com>
2025-04-30 15:51:18 +0530

    Add files via upload

---
+++ Customer Retention Analysis/customer_retention_analysis.sql
@@ -0,0 +1,6 @@
+SELECT strftime('%Y-%m', o.order_date) AS order_month, COUNT(DISTINCT o.customer_id) AS active_customers
+FROM customer_orders o
+JOIN payments p ON p.order_id = o.order_id
+WHERE p.payment_status = 'completed'
+GROUP BY order_month
+ORDER BY order_month;
