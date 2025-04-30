0f45bf4c892b20be3a8f80b8227707380cc68991
Prithviraj Ghorpade <prithvighorpade653@gmail.com>
2025-04-30 17:08:12 +0530

    Update customer_retention_analysis.sql

--- Customer Retention Analysis/customer_retention_analysis.sql
+++ Customer Retention Analysis/customer_retention_analysis.sql
@@ -1,3 +1,4 @@
+/*Number of customers made repeated purchases in subsequent months*/
 SELECT strftime('%Y-%m', o.order_date) AS order_month, COUNT(DISTINCT o.customer_id) AS active_customers
 FROM customer_orders o
 JOIN payments p ON p.order_id = o.order_id
